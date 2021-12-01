5a4d7897e861d08fab1f48df3bd8fde47a7a5d99
ThaiVu54 <thai01654369440@gmail.com>
2021-12-01 15:28:35 +0700

    @valid

--- src/main/java/validation/model/PhoneNumber.java
+++ src/main/java/validation/model/PhoneNumber.java
@@ -1,25 +1,19 @@
 package validation.model;
-
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+@Component
 public class PhoneNumber implements Validator {
+
     private String number;
-
-    public PhoneNumber() {
-
-    }
 
     public String getNumber() {
         return number;
     }
 
     public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public PhoneNumber(String number) {
         this.number = number;
     }
 
@@ -30,16 +24,17 @@ public class PhoneNumber implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+
         PhoneNumber phoneNumber = (PhoneNumber) target;
         String number = phoneNumber.getNumber();
         ValidationUtils.rejectIfEmpty(errors, "number", "number.empty");
-        if (number.length() > 11 || number.length() < 10) {
+        if (number.length()>11 || number.length()<10){
             errors.rejectValue("number", "number.length");
         }
-        if (!number.startsWith("0")) {
+        if (!number.startsWith("0")){
             errors.rejectValue("number", "number.startsWith");
         }
-        if (!number.matches("(^$|[0-9]*$)")) {
+        if (!number.matches("(^$|[0-9]*$)")){
             errors.rejectValue("number", "number.matches");
         }
     }
